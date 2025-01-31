8af39ea35eedca15b03bc9f49de2fe464af18cfc
Astghik Hovhannisyan <astxofficial@gmail.com>
2025-01-31 15:31:46 +0400

    update phone number

--- src/constants/company-data.js
+++ src/constants/company-data.js
@@ -1,8 +1,8 @@
 const FACEBOOK_ACCOUNT = 'https://www.facebook.com/profile.php?id=100038463386333';
 const INSTA_ACCOUNT = 'https://www.instagram.com/khimmaster_am?igsh=MTZ1OXRubnRmNTlvYQ==';
-const PHONE_NUMBER_ONE = '+374 33 48 48 33';
+const PHONE_NUMBER_ONE = '+374 93 875 875';
 const PHONE_NUMBER_TWO = '';
-const PHONE_NUMBER_FORMATTED_ONE = '+374 33 48 48 33';
+const PHONE_NUMBER_FORMATTED_ONE = '+374 93 875 875';
 const PHONE_NUMBER_FORMATTED_TWO = '';
 const ADDRESS = 'address_details';
 const COMPANY_EMAIL = 'info@wastewater.am';
